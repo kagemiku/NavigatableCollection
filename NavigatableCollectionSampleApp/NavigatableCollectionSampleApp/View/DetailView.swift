ee133cee3ff49a3bef13a0023cc5cf2efc8a1e03
kagemiku <kagemiku@gmail.com>
2020-03-28 18:53:23 +0900

    Add simple DetailView to sample project

---
+++ NavigatableCollectionSampleApp/NavigatableCollectionSampleApp/View/DetailView.swift
@@ -0,0 +1,28 @@
+//
+//  DetailView.swift
+//  NavigatableCollectionSampleApp
+//
+//  Created by kagemiku on 2020/03/28.
+//  Copyright © 2020 kagemiku. All rights reserved.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+
+    private let id: Int
+
+    var body: some View {
+        Text("ID: \(id)")
+    }
+
+    init(id: Int) {
+        self.id = id
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(id: 0)
+    }
+}
