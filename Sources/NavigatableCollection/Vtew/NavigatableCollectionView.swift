7619fe239458030df866b13e9d6003a14fc222e0
kagemiku <kagemiku@gmail.com>
2020-03-28 21:46:56 +0900

    Add RowData private struct, and ditribute each dataset to row and column

--- Sources/NavigatableCollection/Vtew/NavigatableCollectionView.swift
+++ Sources/NavigatableCollection/Vtew/NavigatableCollectionView.swift
@@ -17,23 +17,33 @@ public struct NavigatableCollectionViewConfig {
 
 public struct NavigatableCollectionView<DataSource, Cell>: View where DataSource: RandomAccessCollection, DataSource.Element: Identifiable, Cell: View {
 
+    private struct RowData: Identifiable {
+        let id: DataSource.Element.ID
+        let columnDataList: [DataSource.Element]
+    }
+
     private let dataSource: [DataSource.Element]
     private let config: NavigatableCollectionViewConfig
     private let cell: (DataSource.Element) -> Cell
 
     public var body: some View {
         List {
-            ForEach(dataSource) { data in
+            ForEach(rows) { row in
                 HStack {
-                    ForEach(0..<3) { _ in
-                        self.cell(data)
+                    ForEach(row.columnDataList) { columnData in
+                        self.cell(columnData)
                     }
                 }
             }
         }
     }
 
-    public init(dataSource: DataSource, @ViewBuilder cell: @escaping (DataSource.Element) -> Cell) {
+    private var rows: [RowData] {
+        dataSource
+            .split(by: config.columns)
+            .map { .init(id: $0.first!.id, columnDataList: $0) }
+    }
+
     public init(dataSource: DataSource, config: NavigatableCollectionViewConfig, @ViewBuilder cell: @escaping (DataSource.Element) -> Cell) {
         self.dataSource = dataSource.map { $0 }
         self.config = config
