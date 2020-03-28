45935a6f19964e7a8a69458801b16414fcc3649f
kagemiku <kagemiku@gmail.com>
2020-03-28 17:48:19 +0900

    Delete default file, and add empty collection view

--- Sources/NavigatableCollection/NavigatableCollection.swift
+++
@@ -1,3 +0,0 @@
-struct NavigatableCollection {
-    var text = "Hello, World!"
-}
---
+++ Sources/NavigatableCollection/NavigatableCollectionView.swift
@@ -0,0 +1,23 @@
+//
+//  NavigatableCollectionView.swift
+//  NavigatableCollection
+//
+//  Created by kagemiku on 2020/03/28.
+//
+
+import SwiftUI
+
+public struct NavigatableCollectionView: View {
+
+    public var body: some View {
+        Text("Hello, World!")
+    }
+
+    public init() { }
+}
+
+struct NavigatableCollectionView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigatableCollectionView()
+    }
+}
