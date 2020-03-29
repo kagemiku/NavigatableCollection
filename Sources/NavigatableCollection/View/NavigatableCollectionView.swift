d6ef7bc9fe92055c37bdf84314ee1fbb410bed48
kagemiku <kagemiku@gmail.com>
2020-03-29 12:36:01 +0900

    Separate file for config

--- Sources/NavigatableCollection/View/NavigatableCollectionView.swift
+++ Sources/NavigatableCollection/View/NavigatableCollectionView.swift
@@ -7,24 +7,6 @@
 
 import SwiftUI
 
-/// A configuration for NavigatableCollectionView
-public struct NavigatableCollectionViewConfig {
-    public let columns: Int
-    public let hSpacing: CGFloat
-    public let rowInsets: EdgeInsets
-
-    /// Creates an instance
-    /// - Parameters:
-    ///   - columns: The number of columns for rows
-    ///   - hSpacing: The horizontal space between cells
-    ///   - rowInsets: The EdgeInsets for rows
-    public init(columns: Int, hSpacing: CGFloat = 0, rowInsets: EdgeInsets = .init()) {
-        self.columns = columns
-        self.hSpacing = hSpacing
-        self.rowInsets = rowInsets
-    }
-}
-
 /// A container that manages an ordered collection of data items and presents them.
 public struct NavigatableCollectionView<DataSource, Cell>: View where DataSource: RandomAccessCollection, DataSource.Element: Identifiable, Cell: View {
 
